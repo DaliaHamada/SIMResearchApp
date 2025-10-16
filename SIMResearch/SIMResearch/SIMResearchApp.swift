ee74fa7532dc8253cc535467b427a1f6a1619834
Dalia Hamada <dalya.hamada@fawry.com>
2025-10-16 14:25:00 +0300

    SIMResearchAppSetup

---
+++ SIMResearch/SIMResearch/SIMResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIMResearchApp.swift
+//  SIMResearch
+//
+//  Created by Dalia Hamada on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SIMResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
